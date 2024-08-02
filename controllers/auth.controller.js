17483ed5686e26b49810230eb8cbba5ac970c45c
HarshKotambe23 <124414399+HarshKotambe23@users.noreply.github.com>
2024-08-02 14:43:30 +0530

    customer login updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -142,7 +142,7 @@ exports.registerCustomer = asyncHandler(async (req, res) => {
 })
 exports.loginCustomer = asyncHandler(async (req, res) => {
     const { mobile, password } = req.body
-    const { isError, error } = checkEmpty({ customer, password })
+    const { isError, error } = checkEmpty({ mobile, password })
     if (isError) {
         return res.status(400).json({ messsage: "All Feilds Required", error })
     }
